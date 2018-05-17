43ca277143f7900913c3224a41b4fc17fe55a289
Scott McAllister <mr.scott.mcallister@gmail.com>
2018-05-16 22:13:25 -0300

    add some assets and player movement

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+	public static SoundManager Instance = null;
+	public AudioClip alienBuzz1;
+	public AudioClip alienBuzz2;
+	public AudioClip alienDies;
+	public AudioClip bulletFire;
+	public AudioClip shipExplosion;
+
+	private AudioSource soundEffectAudio;
+
+	// Use this for initialization
+	void Start () {
+		if (Instance == null) {
+			Instance = this;
+		} else if (Instance != this) {
+			Destroy(gameObject);
+		}
+
+		soundEffectAudio = GetComponent<AudioSource>();
+	}
+	
+	public void playOneShot(AudioClip clip) {
+		soundEffectAudio.PlayOneShot(clip);
+	}
+}
---
+++ Assets/Scripts/SpaceShip.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShip : MonoBehaviour {
+
+	public float shipSpeed = 30;
+
+	// public GameObject theBullet;
+
+	void FixedUpdate() {
+		float horzMove = Input.GetAxisRaw("Horizontal");
+
+		GetComponent<Rigidbody2D>().velocity = new Vector2 (horzMove, 0) * shipSpeed;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Jump")) {
+			
+		}
+	}
+}
